91399859d4326f2b852fc1361fa219b4e9eeff19
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2022-12-17 18:21:42 -0500

    Created the files

---
+++ objects.js
@@ -0,0 +1,15 @@
+let cat = {
+    name: "Cleo",
+    age: 4,
+    foods: {
+        favourite: "Tuna",
+        leastFavourite: "Oranges"
+    },
+    eyeColor: "blue",
+    meow() {
+        alert("Meowwwwwww")
+    }
+}
+
+cat.foods.leastFavourite
+
