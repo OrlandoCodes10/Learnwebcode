fc47145da2a800f43a567355b795704df2420505
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2022-12-17 18:43:54 -0500

    Collections of values, examples of array

---
+++ Day 4/arrays.js
@@ -0,0 +1,4 @@
+let myFavouriteNumber = 7
+let myFavouriteNumbers = [9, 2, 8, 3, 7, 4]
+let myWords = ["red", "orange", "yellow"]
+let myPets = [{name: "Meowsalot", species: "cat"}, {name: "Barksalot", species: "dog"}]
