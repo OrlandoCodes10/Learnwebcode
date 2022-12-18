21fa71ac3a9bdfcb430b2b8c879708b9ff628f89
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2022-12-17 19:15:50 -0500

    Completed Arrays

--- Day 4/arrays.js
+++ Day 4/arrays.js
@@ -2,3 +2,9 @@ let myFavouriteNumber = 7
 let myFavouriteNumbers = [9, 2, 8, 3, 7, 4]
 let myWords = ["red", "orange", "yellow"]
 let myPets = [{name: "Meowsalot", species: "cat"}, {name: "Barksalot", species: "dog"}]
+
+myWords.push("green")
+console.log("red")
+
+let myName = "Orlando"
+console.log(myName.toUpperCase)
