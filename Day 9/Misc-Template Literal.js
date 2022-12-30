282852f4fd0a0d1e7c319b9ceae6e71a29953b09
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2022-12-30 00:29:37 -0500

    semicolons example

--- Day 9/Misc-Template Literal.js
+++ Day 9/Misc-Template Literal.js
@@ -1,6 +1,4 @@
-let myName = "Brad"
-
-console.log(`Hello, my name is ${myName} and the sky is blue.`)
+let myName = "Brad";console.log(`Hello, my name is ${myName} and the sky is blue.`)
 
 console.log(myName)
 
