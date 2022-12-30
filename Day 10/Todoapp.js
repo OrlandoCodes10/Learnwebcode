d2b014afdeec1fc71d26bba25f43cbfe6dd0185a
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2022-12-30 00:49:50 -0500

    Connecting JS

--- Day 10/Todoapp.js
+++ Day 10/Todoapp.js
@@ -1,1 +1,6 @@
-document.getElementById("ourForm")
+let ourForm = document.getElementById("ourForm")
+ourForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    alert("Thanks for clicking the form")
+})
+
