0ee246a624dccabdbb0a72255831b8b397eeb574
OrlandoCodes10 <orlandofranco.novesteras@gmail.com>
2022-12-31 01:35:48 -0500

    Finishing up Todo app

--- Day 10/Todoapp.js
+++ Day 10/Todoapp.js
@@ -1,6 +1,12 @@
 let ourForm = document.getElementById("ourForm")
+let ourField = document.getElementById("ourField")
+let ourList = document.getElementById("ourList")
+
 ourForm.addEventListener("submit", (e) => {
     e.preventDefault()
-    alert("Thanks for clicking the form")
+    console.log(ourField.value)
 })
 
+function createItem(x) {
+    ourList.insertAdjacentHTML("beforeend", x)
+}
